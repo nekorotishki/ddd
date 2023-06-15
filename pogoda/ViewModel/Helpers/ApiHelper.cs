d4295a2a77462dc91570631ccd35fe01680fcb7a
Даниил <danildergachevfox@gmail.com>
2023-06-15 13:05:13 +0300

    add api helper

---
+++ pogoda/ViewModel/Helpers/ApiHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+
+namespace pogoda.ViewModel.Helpers
+{
+    class ApiHelper
+    {
+        private static string url = "";
+        //public static string put(string json) 
+        //{
+        //    try 
+        //    {
+        //        HttpClient client = new HttpClient();
+        //        HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+        //    }
+        //    catch (Exception e) {
+        //        return e.Message;
+        //    }
+        //}
+    }
+}
