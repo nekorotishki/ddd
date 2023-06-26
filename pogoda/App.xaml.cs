2ae1cc46a40ff33f938dca6a5e38948e3775db25
AZRV17 <alex170805azrov@gmail.com>
2023-06-26 13:47:43 +0300

    ListView and switch theme

--- pogoda/App.xaml.cs
+++ pogoda/App.xaml.cs
@@ -13,5 +13,42 @@ namespace pogoda
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            DateTime currentTime = DateTime.Now;
+
+            string theme = GetThemeByTime(currentTime);
+
+            ResourceDictionary themeDictionary = new ResourceDictionary
+            {
+                Source = new Uri($"pack://application:,,,/Themes;component/Themes/{theme}.xaml")
+            };
+
+            Resources.MergedDictionaries.Add(themeDictionary);
+        }
+
+        private string GetThemeByTime(DateTime currentTime)
+        {
+            int hour = currentTime.Hour;
+
+            if (hour >= 0 && hour <= 3)
+            {
+                return "Night";
+            }
+            else if (hour >= 4 && hour <= 11)
+            {
+                return "Morning";
+            }
+            else if (hour >= 12 && hour <= 16)
+            {
+                return "Day";
+            }
+            else
+            {
+                return "Morning";
+            }
+        }
     }
 }
